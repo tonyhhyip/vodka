8a1dd567e50806f7a5f84a3cc2f7e80247da1c97
Tony Yip <tony@opensource.hk>
2017-12-26 05:20:44 +0800

    Create default error type

---
+++ errors/default.go
@@ -0,0 +1,27 @@
+package errors
+
+func NewError(source error, errorType ErrorType, meta map[string]interface{}) Error {
+	return &defaultError{
+		source:    source,
+		errorType: errorType,
+		meta:      meta,
+	}
+}
+
+type defaultError struct {
+	source    error
+	errorType ErrorType
+	meta      map[string]interface{}
+}
+
+func (e *defaultError) GetSource() error {
+	return e.source
+}
+
+func (e *defaultError) GetType() ErrorType {
+	return e.errorType
+}
+
+func (e *defaultError) GetMeta() map[string]interface{} {
+	return e.meta
+}

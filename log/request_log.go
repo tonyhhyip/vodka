3eef2e729721d7f8630c992116d1a6c93d2af4ef
Tony Yip <tony@opensource.hk>
2017-12-27 06:38:26 +0800

    Add content length count

--- log/request_log.go
+++ log/request_log.go
@@ -1,8 +1,11 @@
 package log
 
 import (
+	"strconv"
+
 	"github.com/sirupsen/logrus"
 	"github.com/tonyhhyip/vodka"
+	"github.com/tonyhhyip/vodka/pkg/plugins"
 )
 
 type RequestLogType int
@@ -18,7 +21,25 @@ type requestLog struct {
 
 func (r *requestLog) createHandler() vodka.Handler {
 	return func(c vodka.Context) {
-		c.Next()
-		r.logger.WithField("context", c).Info()
+		ctx := &sizeCountContext{
+			ContextWrapper: plugins.WrapContext(c),
+		}
+		c.Next(ctx)
+		r.logger.WithField("context", ctx).Info()
 	}
+}
+
+type sizeCountContext struct {
+	plugins.ContextWrapper
+	size int
+}
+
+func (c *sizeCountContext) Data(data []byte) {
+	c.size += len(data)
+	c.ContextWrapper.Data(data)
+}
+
+func (c *sizeCountContext) Abort() {
+	c.Header("Content-Length", strconv.FormatInt(int64(c.size), 10))
+	c.ContextWrapper.Abort()
 }

cb918cc6ed47e53668de21b6b279beb337a54dd3
Tony Yip <tony@opensource.hk>
2017-12-27 00:24:02 +0800

    Craft for router

--- route/types.go
+++ route/types.go
@@ -3,5 +3,12 @@ package route
 import "github.com/tonyhhyip/vodka"
 
 type Router interface {
-	Match(method string, path string) vodka.Handler
+	Match(method vodka.Method, path string) vodka.Handler
+}
+
+func WrapRouter(router Router) vodka.Handler {
+	return func(c vodka.Context) {
+		handler := router.Match(c.GetMethod(), c.GetPath())
+		handler(c)
+	}
 }

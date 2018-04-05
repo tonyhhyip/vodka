5c8ed84a1ddfdff0616eeb49c659c8133ec16e92
Tony Yip <tony@opensource.hk>
2018-04-06 03:26:37 +0800

    Add comment doc

--- server.go
+++ server.go
@@ -50,6 +50,7 @@ func (srv *Server) ListenAndServeTLS(certFile, keyFile string, handler Handler) 
 	return srv.Server.ListenAndServeTLS(certFile, keyFile)
 }
 
+// StandBy setup everything required for running a server
 func (srv *Server) StandBy(handler Handler) {
 	srv.Server.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		base, cancel := context.WithTimeout(context.Background(), srv.Timeout)

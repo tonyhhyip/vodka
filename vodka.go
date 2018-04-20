32ac893812ce11e94b3b7df8dbb41d77d11cc28e
Tony Yip <tony@opensource.hk>
2018-04-20 10:38:02 +0800

    Update license

--- vodka.go
+++ vodka.go
@@ -1,3 +1,6 @@
+// Copyright 2018 Tony Yip. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
 package vodka
 
 const Version = "2.2.0"

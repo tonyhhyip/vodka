a4b9541e9488d7e3d589fc3bde18b3b6ca66130b
Tony Yip <tony@opensource.hk>
2018-04-29 13:32:27 +0800

    Add casting of handler

---
+++ http.go
@@ -0,0 +1,44 @@
+package vodka
+
+import (
+	"context"
+	"net/http"
+)
+
+// CastHandlerForHTTP convert vodka.Handle to http.Handler
+func CastHandlerForHTTP(h Handler, logger Logger) *handler {
+	return &handler{
+		Handler: h,
+		Logger:  logger,
+	}
+}
+
+type handler struct {
+	Handler
+	Logger Logger
+}
+
+// ServeHTTP serve HTTP request
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	base := r.Context()
+	base, cancel := context.WithCancel(base)
+	defer cancel()
+	ctx := NewContextWithLogger(base, h.Logger, w, r)
+
+	done := make(chan bool)
+
+	go func(ctx *Context, done chan bool) {
+		h.Handle(ctx)
+		done <- true
+	}(ctx, done)
+
+	select {
+	case <-done:
+		break
+	case <-ctx.Done():
+		if err := ctx.Err(); err == context.DeadlineExceeded {
+			http.Error(w, "Timeout", http.StatusGatewayTimeout)
+		}
+		break
+	}
+}
